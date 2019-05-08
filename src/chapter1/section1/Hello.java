3a18cfab9f953e29de025d99cb1b05c2f3cf40d4
CY <614672958@qq.com>
2019-05-08 23:00:52 +0800

    init

---
+++ src/chapter1/section1/Hello.java
@@ -0,0 +1,25 @@
+package chapter1.section1;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = a+2;
+
+        System.out.println(b);
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+        aa();
+        System.out.println("hello java");
+
+    }
+
+    public static void aa() {
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+
+    }
+}
